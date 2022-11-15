6e360e663c7759f526c954190fd626da5d06a19f
durakrd <115124360+durakrd@users.noreply.github.com>
2022-11-14 17:05:36 -0800

    Add files via upload
    scatterplot matrix file

---
+++ source/scatterplot_matrix.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(GGally)
+
+source("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/source/aggregate_table.R")
+
+matrix_df <- final_aggregate_df %>%
+  select(c("biden_percent", "trump_percent", "low_mask", "vaccine_hesitant", "deaths_per_1000000"))
+
+ggpairs(
+  matrix_df,
+  title = "Scatter Plot Matrix by State",
+  diag = list(continuous = wrap("barDiag")),
+  columnLabels = c(
+    "Votes for Biden (%)",
+    "Votes for Trump (%)",
+    "Low Mask Use (%)", 
+    "Vaccine Hesitancy (%)",
+    "Covid Deaths (per 100,000)"
+    )
+  )
