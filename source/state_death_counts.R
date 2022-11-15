506c50c6e71bc12a4c16ac9a7be1eb3194b5a230
durakrd <krithik.duraisamy@gmail.com>
2022-11-14 23:55:16 -0800

    final fixes

--- source/state_death_counts.R
+++ source/state_death_counts.R
@@ -3,9 +3,11 @@ library(tidyverse)
 source("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/source/aggregate_table.R")
 
 state_death_counts <- final_aggregate_df %>%
-  select(c("State", "deaths_per_100000"))
+  select(c("deaths_per_100000", "State"))
 
 death_count_plot <- function() {
   ggplot(data = state_death_counts) +
-    geom_col(mapping = aes(x = deaths_per_100000 , y = State))
+    geom_col(mapping = aes(deaths_per_100000 , reorder(State, -deaths_per_100000))) +
+    labs(title="Histogram for COVID Death Rates by State") +
+    labs(x="COVID Deaths per 100,000", y="States")
 }
