56bcc2093819f7d7dd86c68a0f2bb3ccb2dcca99
durakrd <krithik.duraisamy@gmail.com>
2022-11-14 18:21:14 -0800

    fixes

--- source/scatterplot_matrix.R
+++ source/scatterplot_matrix.R
@@ -6,15 +6,17 @@ source("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main
 matrix_df <- final_aggregate_df %>%
   select(c("biden_percent", "trump_percent", "low_mask", "vaccine_hesitant", "deaths_per_1000000"))
 
-ggpairs(
-  matrix_df,
-  title = "Scatter Plot Matrix by State",
-  diag = list(continuous = wrap("barDiag")),
-  columnLabels = c(
-    "Votes for Biden (%)",
-    "Votes for Trump (%)",
-    "Low Mask Use (%)", 
-    "Vaccine Hesitancy (%)",
-    "Covid Deaths (per 100,000)"
+matrix_plot <- function() {
+  ggpairs(
+    matrix_df,
+    title = "Scatter Plot Matrix by State",
+    diag = list(continuous = wrap("barDiag")),
+    columnLabels = c(
+      "Votes for Biden (%)",
+      "Votes for Trump (%)",
+      "Low Mask Use (%)", 
+      "Vaccine Hesitancy (%)",
+      "Covid Deaths (per 100,000)"
     )
   )
+}
--- source/state_death_counts.R
+++ source/state_death_counts.R
@@ -2,7 +2,10 @@ library(tidyverse)
 
 source("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/source/aggregate_table.R")
 
-View(final_aggregate_df)
-
 state_death_counts <- final_aggregate_df %>%
   select(State, deaths_per_1000000)
+
+death_count_plot <- function() {
+  ggplot(data = state_death_counts) +
+    geom_col(mapping = aes(x = deaths_per_1000000 , y = State))
+}
