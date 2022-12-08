7608c6c8e16736017f808c38168991990d665402
durakrd <115124360+durakrd@users.noreply.github.com>
2022-12-07 21:05:24 -0800

    Update app_server.R

--- docs/app_server.R
+++ docs/app_server.R
@@ -2,67 +2,68 @@ library(shiny)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
-library(plotly)
 
-shiny_state_df <- function() {
-  mask_df <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/data/mask-use-by-county.csv")
-  state_df <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/data/Vaccine_Hesitancy_for_COVID-19__County_and_local_estimates.csv")
-  state_df <- state_df %>%
-    select(c("FIPS.Code", "State"))
-  mask_df <- mask_df %>%
+source("../source/scatterplot_matrix.R")
+
+mask_df <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/data/mask-use-by-county.csv")
+state_df <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/data/Vaccine_Hesitancy_for_COVID-19__County_and_local_estimates.csv")
+state_df <- state_df %>%
+  select(c("FIPS.Code", "State"))
+shiny_state_df <- mask_df %>%
     left_join(state_df, by=c("COUNTYFP" = "FIPS.Code")) %>%
     group_by(State) %>%
     summarize(Never = mean(NEVER, na.rm=TRUE),
               Rarely = mean(RARELY, na.rm=TRUE),
               Sometimes = mean(SOMETIMES, na.rm=TRUE),
-              Frequently = mean(FREQUENTLY, na.rm=TRUE)
+              Frequently = mean(FREQUENTLY, na.rm=TRUE),
+              Always = mean(ALWAYS, na.rm=TRUE)
     ) %>%
     ungroup()
-}
-
-source("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/source/aggregate_table.R")
 
 server <- function(input, output) {
 
+  # Function selects for state in reactive function
+  state_mask_func <- reactive({
+    return_vector <- shiny_state_df %>%
+      filter(State == input$state) %>%
+      select(c("Never", "Rarely", "Sometimes", "Frequently", "Always"))
+  })
+  
+  # Function returns pie chart for mask usage depending on State selected
   output$piechart <- renderPlot ({
-  #make_pie_chart <-
-    covid <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/data/mask-use-by-county.csv")
-    
-    national_mask_use <- covid %>%
-      select(c("Never", "Rarely", "Sometimes", "Frequently", "Always")) %>%
-      colMeans()
+    #make_pie_chart <-
+    state_usage <- state_mask_func()
+    state_vect <- as.numeric(as.vector(state_usage[1,]))
     
     lbls <- c("Never", "Rarely", "Sometimes", "Frequently", "Always")
-    pct <- round(national_mask_use/sum(national_mask_use)*100)
+    pct <- round(state_vect/sum(state_vect)*100)
     lbls <- paste(lbls, pct)
     lbls <- paste0(lbls, "%")
-    pie(national_mask_use, labels = lbls, col = rainbow(length(lbls)),
-        main="National Mask Usage During COVID Pandemic")
+    pie(state_vect, labels = lbls, col = rainbow(length(lbls)),
+        main=paste(input$state, "Mask Usage During COVID Pandemic"))
+  })
+
+  # Functions pulls values corresponding to x value
+  x_func <- reactive({
+    matrix_df %>%
+      pull(input$x_var)
+  })
+
+  # Functions pulls values corresponding to y value
+  y_func <- reactive({
+    matrix_df %>%
+      pull(input$y_var)
   })
   
-  
-  state_death_data <- reactive({
-    final_aggregate_df %>%
-    filter(deaths_per_100000 > input$mindepth) %>%
-    filter(deaths_per_100000 < input$maxdepth) %>%
-    select(c("deaths_per_100000", "State"))
-})
-
-  output$plot <- renderPlot({
-
-    ggplot(state_death_data()) +
-      geom_col(mapping = aes(deaths_per_100000, reorder(State, -deaths_per_100000), fill = deaths_per_100000)) +
-      #xlim(input$mindepth, input$maxdepth) +
-      labs(title="Histogram for COVID Death Rates by State") +
-      labs(x="COVID Deaths per 100,000", y="States")
+  # Plots scatter plot with given x and y value from app
+  output$scat_plot <- renderPlot({
+    ggplot(matrix_df, mapping = aes(x = x_func(), y = y_func())) + 
+      geom_point() +
+      geom_smooth() +
+      labs( 
+        title = paste(input$x_var, "v.s.", input$y_var),
+        x = toupper(input$x_var),
+        y = toupper(input$y_var)
+        )
   })
-  
 }
-
-
-
-
-
-
-
-
