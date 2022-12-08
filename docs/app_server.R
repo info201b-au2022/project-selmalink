dc77fd818224dab760a1f2e747dca8b2869e60e3
durakrd <115124360+durakrd@users.noreply.github.com>
2022-12-07 21:35:40 -0800

    Update app_server.R

--- docs/app_server.R
+++ docs/app_server.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
-source("../source/scatterplot_matrix.R")
+source("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/source/scatterplot_matrix.R")
 source("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/source/aggregate_table.R")
 
 mask_df <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-selmalink/main/data/mask-use-by-county.csv")
